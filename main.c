6d36e0ad660b88e04f154176aa45eb317e9417e4
João Nunes <joaovictor31415@gmail.com>
2023-11-07 16:41:41 -0300

    Corrigindo erros de caracteres especiais no CMD

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <time.h>
 
-// Criando Macros para usar no decorer do cÃ³digo
+// Criando Macros para usar no decorer do código
 
 // Macros para calculos de meio de transporte
-// IMPORTANTE - Dados extraidos da internet como referÃªncia
+// IMPORTANTE - Dados extraidos da internet como referência
 #define INDICE_DESEMPREGO 8.0
 #define POPULACAO_INFANTIL 17.0 
 #define INDICE_APOSENTADORIA 15.0
@@ -16,51 +17,51 @@
 
 typedef struct Casa {
     int ID;             // Nome da casa
-    int numMoradores;   // NÃºmero de moradores na casa
-    double gastoEletrico; // Gasto elÃ©trico da casa em kWh
-    double gastoAgua;    // Gasto de Ã¡gua da casa em litros
+    int numMoradores;   // Número de moradores na casa
+    double gastoEletrico; // Gasto elétrico da casa em kWh
+    double gastoAgua;    // Gasto de água da casa em litros
 }Casa;
 
 typedef struct Rua {
     int ID;             // Nome da rua
-    double comprimento; // Comprimento da rua em quilÃ´metros
-    int anoObra;        // Ano de inauguraÃ§Ã£o da Obra
-    int numCasas;       // NÃºmero de casas na rua
-    struct Casa casas[20]; // Vetor de casas na rua (atÃ© 20 casas)
+    double comprimento; // Comprimento da rua em quilômetros
+    int anoObra;        // Ano de inauguração da Obra
+    int numCasas;       // Número de casas na rua
+    struct Casa casas[20]; // Vetor de casas na rua (até 20 casas)
 }Rua;
 
 typedef struct Onibus {
-    int ID;           // IdentificaÃ§Ã£o do Ã´nibus
-    int capacidade;   // Capacidade de passageiros do Ã´nibus
+    int ID;           // Identificação do ônibus
+    int capacidade;   // Capacidade de passageiros do ônibus
 } Onibus;
 
 typedef struct LinhaOnibus {
-    int ID;             // IdentificaÃ§Ã£o da linha de Ã´nibus
-    struct Rua *inicio; // Rua de inÃ­cio da linha
-    struct Rua *fim;    // Rua de tÃ©rmino da linha
-    struct Onibus onibus; // Ã”nibus que opera na linha
+    int ID;             // Identificação da linha de ônibus
+    struct Rua *inicio; // Rua de início da linha
+    struct Rua *fim;    // Rua de término da linha
+    struct Onibus onibus; // Ônibus que opera na linha
 } LinhaOnibus;
 
 typedef struct {
     int ID; 
-    double area; // Ãrea em KM quadrados do distrito
-    int habitantes; // NÃºm. de habitantes
+    double area; // Área em KM quadrados do distrito
+    int habitantes; // Núm. de habitantes
     struct Rua ruas[50]; // Vetor de ruas de cada distrito
     int desempregados; 
-    int populacaoInfantil;// NÃºm. de habitantes menores de 17 anos
+    int populacaoInfantil;// Núm. de habitantes menores de 17 anos
     int aposentados; 
-    double densidadeHabitacional; // NÃºm. Habitantes / Ãrea
+    double densidadeHabitacional; // Núm. Habitantes / Área
     int carros; // Estimativa de quantos carros podem existir no distrito simulado - 1 a cada 10 pessoas
     int motos; // Estimativa de quantas motos podem existir no distrito simulado - 1 a cada 20 pessoas
-    struct LinhaOnibus linhasOnibus[10]; // Vetor de linhas de Ã´nibus
+    struct LinhaOnibus linhasOnibus[10]; // Vetor de linhas de ônibus
 } Distrito;
 
-// FunÃ§Ã£o para gerar um nÃºmero aleatÃ³rio entre min e max
+// Função para gerar um número aleatório entre min e max
 int randomInRange(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
-// FunÃ§Ã£o para criar uma casa
+// Função para criar uma casa
 void criarCasa(struct Casa *casa, int ID, int numMoradores, double gastoEletrico, double gastoAgua) {
     casa->ID = ID;
     casa->numMoradores = numMoradores;
@@ -68,7 +69,7 @@ void criarCasa(struct Casa *casa, int ID, int numMoradores, double gastoEletrico
     casa->gastoAgua = gastoAgua;
 }
 
-// FunÃ§Ã£o para criar uma rua
+// Função para criar uma rua
 void criarRua(struct Rua *rua, int ID, double comprimento, int anoObra, int numCasas) {
     rua->ID = ID;
     rua->comprimento = comprimento;
@@ -77,7 +78,7 @@ void criarRua(struct Rua *rua, int ID, double comprimento, int anoObra, int numC
 
     for (int i = 0; i < numCasas; i++) {
         int casaID = i + 1;
-        int numMoradores = randomInRange(1, 5);  // NÃºmero de moradores (1 a 5)
+        int numMoradores = randomInRange(1, 5);  // Número de moradores (1 a 5)
         double gastoEletrico = numMoradores * MEDIA_GASTO_ENERGIA; 
         double gastoAgua = numMoradores * MEDIA_GASTO_AGUA;
 
@@ -85,7 +86,7 @@ void criarRua(struct Rua *rua, int ID, double comprimento, int anoObra, int numC
     }
 }
 
-// FunÃ§Ã£o para criar e inicializar um distrito
+// Função para criar e inicializar um distrito
 void criarDistrito(Distrito *distrito, int ID, double area, int habitantes, int numRuas) {
     distrito->ID = ID;
     distrito->area = area;
@@ -97,7 +98,7 @@ void criarDistrito(Distrito *distrito, int ID, double area, int habitantes, int 
     distrito->carros = habitantes / 10;
     distrito->motos = habitantes / 20;
 
-    // Calcula o nÃºmero total de casas com base no nÃºmero de habitantes
+    // Calcula o número total de casas com base no número de habitantes
     int numCasas = habitantes / MEDIA_MORADORES_RESIDENCIA;
 
     // Inicializa o comprimento total das ruas
@@ -107,39 +108,39 @@ void criarDistrito(Distrito *distrito, int ID, double area, int habitantes, int 
     for (int i = 0; i < numRuas; i++) {
         int ruaID = i + 1;
 
-        // Calcula o mÃ¡ximo comprimento permitido para esta rua
-        double comprimentoMaximo = (area / numRuas) * 2; // Exemplo: divide a Ã¡rea igualmente entre as ruas e multiplica por 2
+        // Calcula o máximo comprimento permitido para esta rua
+        double comprimentoMaximo = (area / numRuas) * 2; // Exemplo: divide a área igualmente entre as ruas e multiplica por 2
 
-        // Calcula o comprimento da rua, garantindo que nÃ£o exceda o mÃ¡ximo
+        // Calcula o comprimento da rua, garantindo que não exceda o máximo
         double comprimento = randomInRange(1, comprimentoMaximo);
 
         // Atualiza o comprimento total das ruas
         comprimentoTotalRuas += comprimento;
 
-        int anoObra = randomInRange(1950, 2020); // Ano de inauguraÃ§Ã£o da obra
+        int anoObra = randomInRange(1950, 2020); // Ano de inauguração da obra
         
-        // Calcula o nÃºmero de casas para esta rua
+        // Calcula o número de casas para esta rua
         int numCasasNaRua = numCasas / numRuas;
 
         // Atribui as casas a esta rua
         criarRua(&distrito->ruas[i], ruaID, comprimento, anoObra, numCasasNaRua);
 
-        // Reduz o nÃºmero de casas restantes
+        // Reduz o número de casas restantes
         numCasas -= numCasasNaRua;
 
-        // Verifica se o comprimento total das ruas ultrapassou a Ã¡rea mÃ¡xima
+        // Verifica se o comprimento total das ruas ultrapassou a área máxima
         if (comprimentoTotalRuas > area) {
             break;
         }
     }
 }
 
-// FunÃ§Ã£o para listar as informaÃ§Ãµes de um distrito
+// Função para listar as informações de um distrito
 void listarInformacoesDistrito(const Distrito *distrito) {
-    printf("\nInformaÃ§Ãµes do Distrito %d:\n", distrito->ID);
-    printf("Ãrea: %.2f kmÂ²\n", distrito->area);
+    printf("\nInformações do Distrito %d:\n", distrito->ID);
+    printf("Área: %.2f km²\n", distrito->area);
     printf("Habitantes: %d\n", distrito->habitantes);
-    printf("Densidade Habitacional: %.3f Hab/KMÂ²\n", distrito->densidadeHabitacional);
+    printf("Densidade Habitacional: %.3f Hab/KM²\n", distrito->densidadeHabitacional);
     printf("Carros estimados no distrito: %d\n", distrito->carros);
     printf("Motos estimadas no distrito: %d\n", distrito->motos);
     printf("Desempregados: %d\n", distrito->desempregados);
@@ -147,28 +148,28 @@ void listarInformacoesDistrito(const Distrito *distrito) {
     printf("Menores de 17 anos: %d\n", distrito->populacaoInfantil);
 }
 
-// FunÃ§Ã£o para listar as ruas de um distrito
+// Função para listar as ruas de um distrito
 void listarRuasDistrito(const Distrito *distrito) {
     printf("\nRuas do Distrito %d:\n", distrito->ID);
     for (int i = 0; i < 50; i++) {
         if (distrito->ruas[i].ID == 0 || distrito->ruas[i].ID <= 0) {
-            break; // Sai do loop se nÃ£o houver mais ruas
+            break; // Sai do loop se não houver mais ruas
         }
-        printf("Rua %d: Comprimento: %.2f km | Ano de Obra: %d | NÃºmero de Casas: %d\n",
+        printf("Rua %d: Comprimento: %.2f km | Ano de Obra: %d | Número de Casas: %d\n",
                distrito->ruas[i].ID, distrito->ruas[i].comprimento, distrito->ruas[i].anoObra, distrito->ruas[i].numCasas);
     }
 }
 
-// FunÃ§Ã£o para listar as informaÃ§Ãµes das casas em um distrito
+// Função para listar as informações das casas em um distrito
 void listarCasasNoDistrito(const Distrito *distrito) {
-    printf("\nInformaÃ§Ãµes das Casas no Distrito %d:\n", distrito->ID);
+    printf("\nInformações das Casas no Distrito %d:\n", distrito->ID);
     for (int i = 0; i < 50; i++) {
         if (distrito->ruas[i].ID == 0 || distrito->ruas[i].ID <= 0) {
-            break; // Sai do loop se nÃ£o houver mais ruas
+            break; // Sai do loop se não houver mais ruas
         }
         printf("Rua %d:\n", distrito->ruas[i].ID);
         for (int j = 0; j < distrito->ruas[i].numCasas; j++) {
-            printf("Casa %d: NÃºmero de Moradores: %d | Gasto ElÃ©trico: %.2f kWh | Gasto de Ãgua: %.2f litros\n",
+            printf("Casa %d: Número de Moradores: %d | Gasto Elétrico: %.2f kWh | Gasto de Água: %.2f litros\n",
                    distrito->ruas[i].casas[j].ID, distrito->ruas[i].casas[j].numMoradores,
                    distrito->ruas[i].casas[j].gastoEletrico, distrito->ruas[i].casas[j].gastoAgua);
         }
@@ -179,45 +180,45 @@ Distrito* iniciaSimulacao(){
     int areaCidade;
     int totalHabitantes;
 
-    printf("Digite a Ã¡rea estimada da cidade em KMÂ²: ");
+    printf("Digite a área estimada da cidade em KM²: ");
     scanf("%d", &areaCidade);
 
     printf("Digite o total estimado de habitantes da cidade: ");
     scanf("%d", &totalHabitantes);
 
-    // Calcula o mÃ­nimo e o mÃ¡ximo de habitantes por distrito
+    // Calcula o mínimo e o máximo de habitantes por distrito
     int minHabitantesPorDistrito = 0.0075 * totalHabitantes;  // 0.75%
     int maxHabitantesPorDistrito = 0.03 * totalHabitantes;   // 3%
     
-    // Calcula o mÃ­nimo e o mÃ¡ximo de Ã¡rea por distrito
+    // Calcula o mínimo e o máximo de área por distrito
     double minAreaPorDistrito = 0.0055 * areaCidade;  // 0.55%
     double maxAreaPorDistrito = 0.13 * areaCidade;    // 13%
 
-    // Calcula o mÃ­nimo e o mÃ¡ximo de distritos possÃ­veis baseados nas estimativas dos habitantes
+    // Calcula o mínimo e o máximo de distritos possíveis baseados nas estimativas dos habitantes
     int minDistritos = totalHabitantes / maxHabitantesPorDistrito;
     int maxDistritos = totalHabitantes / minHabitantesPorDistrito;
 
-    printf("MÃ­nimo de habitantes por distrito: %d\n", minHabitantesPorDistrito);
-    printf("MÃ¡ximo de habitantes por distrito: %d\n", maxHabitantesPorDistrito);
-    printf("MÃ­nimo de distritos possÃ­veis: %d\n", minDistritos);
-    printf("MÃ¡ximo de distritos possÃ­veis: %d\n", maxDistritos);
+    printf("Mínimo de habitantes por distrito: %d\n", minHabitantesPorDistrito);
+    printf("Máximo de habitantes por distrito: %d\n", maxHabitantesPorDistrito);
+    printf("Mínimo de distritos possíveis: %d\n", minDistritos);
+    printf("Máximo de distritos possíveis: %d\n", maxDistritos);
 
-    // Recebe o nÃºmero de distritos do usuÃ¡rio e verifica se estÃ¡ dentro do limite
+    // Recebe o número de distritos do usuário e verifica se está dentro do limite
     int numDistritos;
-    printf("Digite o nÃºmero de distritos que deseja criar: ");
+    printf("Digite o número de distritos que deseja criar: ");
     scanf("%d", &numDistritos);
 
     if (numDistritos < minDistritos || numDistritos > maxDistritos) {
-        printf("O nÃºmero de distritos deve estar entre %d e %d\n", minDistritos, maxDistritos);
+        printf("O número de distritos deve estar entre %d e %d\n", minDistritos, maxDistritos);
         return 1;
     }
 
     // Distribui aleatoriamente os habitantes entre os distritos
-    srand(time(NULL));  // Inicializa a semente para nÃºmeros aleatÃ³rios
+    srand(time(NULL));  // Inicializa a semente para números aleatórios
 
 
 
-    // Cria um vetor de structs Distritos para a atribuiÃ§Ã£o de dados;
+    // Cria um vetor de structs Distritos para a atribuição de dados;
     Distrito* distritos = malloc(sizeof(Distrito) * numDistritos);
 
     for (int i = 0; i < numDistritos; i++) {
@@ -233,9 +234,12 @@ Distrito* iniciaSimulacao(){
 
 int main() {
 
+    // Corrigindo problemas com caracteres no CMD
+    setlocale(LC_ALL, "portuguese");
+
     Distrito* distritos = iniciaSimulacao();
 
-    // Liberando a memÃ³ria alocada para o vetor de distritos quando nÃ£o for mais necessÃ¡rio.
+    // Liberando a memória alocada para o vetor de distritos quando não for mais necessário.
     free(distritos);
 
     return 0;
